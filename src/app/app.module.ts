70c145216ca6c396339f0237b78880d3dbfc650a
Sergey Pashnin <sspinit88@gmail.com>
2019-11-21 22:13:09 +0300

    tab-component - start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuFaInputComponent } from './lib/au-fa-input/au-fa-input.component';
 import { InputRefDirective } from './lib/au-fa-input/input-ref.directive';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuFaInputComponent,
-    InputRefDirective
+    InputRefDirective,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
