f396426a810c91a7e33e98fa24934ca364d5a64a
Sergey Pashnin <sspinit88@gmail.com>
2019-11-29 23:37:11 +0300

    стартовая ветка

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuFaInputComponent } from './lib/au-fa-input/au-fa-input.component';
-import { InputRefDirective } from './lib/au-fa-input/input-ref.directive';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuFaInputComponent,
-    InputRefDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
