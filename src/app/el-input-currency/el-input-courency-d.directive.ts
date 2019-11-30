834e715311414b8168764ada1a1bbf64a668bf45
Sergey Pashnin <sspinit88@gmail.com>
2019-12-01 00:43:43 +0300

    не идеальный вариант

--- src/app/el-input-currency/el-input-courency-d.directive.ts
+++ src/app/el-input-currency/el-input-courency-d.directive.ts
@@ -49,7 +49,7 @@ export class ElInputCourencyDDirective implements OnInit, AfterContentInit, Afte
 
     this.input.value = inputValue;
 
-    // const value = inputValue.replace(/\s+/g, '')ж
+    // const value = inputValue.replace(/\s+/g, '')
   }
 
   addInputClass(): void {
